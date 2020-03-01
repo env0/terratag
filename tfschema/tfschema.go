f869851ce4f271f835fbca74d212b6f4b17a6c98
Roni Frantchi <roni-frantchi@users.noreply.github.com>
2020-03-01 16:56:26 +0200

    skip any non-aws resources (#15)

--- tfschema/tfschema.go
+++ tfschema/tfschema.go
@@ -5,36 +5,39 @@ import (
 	"github.com/env0/terratag/errors"
 	"github.com/mitchellh/mapstructure"
 	"os/exec"
+	"strings"
 )
 
 func IsTaggable(dir string, resourceType string) (bool, bool) {
-	command := exec.Command("tfschema", "resource", "show", "-format=json", resourceType)
-	command.Dir = dir
-	output, err := command.Output()
-	outputAsString := string(output)
-	errors.PanicOnError(err, &outputAsString)
-
-	var schema map[string]interface{}
-
-	err = json.Unmarshal(output, &schema)
-	errors.PanicOnError(err, nil)
-
 	isTaggable := false
 	isTaggableViaSpecialTagBlock := false
 
-	attributes := schema["attributes"].([]interface{})
-	for _, attributeMap := range attributes {
-		var attribute TfSchemaAttribute
-		err := mapstructure.Decode(attributeMap, &attribute)
+	if strings.HasPrefix(resourceType, "aws_") {
+		command := exec.Command("tfschema", "resource", "show", "-format=json", resourceType)
+		command.Dir = dir
+		output, err := command.Output()
+		outputAsString := string(output)
+		errors.PanicOnError(err, &outputAsString)
+
+		var schema map[string]interface{}
+
+		err = json.Unmarshal(output, &schema)
 		errors.PanicOnError(err, nil)
 
-		if attribute.Name == "tags" {
-			isTaggable = true
-		}
-	}
+		attributes := schema["attributes"].([]interface{})
+		for _, attributeMap := range attributes {
+			var attribute TfSchemaAttribute
+			err := mapstructure.Decode(attributeMap, &attribute)
+			errors.PanicOnError(err, nil)
 
-	if resourceType == "aws_autoscaling_group" {
-		isTaggableViaSpecialTagBlock = true
+			if attribute.Name == "tags" {
+				isTaggable = true
+			}
+		}
+
+		if resourceType == "aws_autoscaling_group" {
+			isTaggableViaSpecialTagBlock = true
+		}
 	}
 
 	return isTaggable, isTaggableViaSpecialTagBlock
