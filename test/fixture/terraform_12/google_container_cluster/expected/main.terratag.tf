e3b52c146e8b63f07ef0bca02d9e1dfdc2fea71b
Yaron Yarimi <yaron.yarimi@env0.com>
2020-07-06 18:34:17 +0300

    add integration tests

---
+++ test/fixture/terraform_12/google_container_cluster/expected/main.terratag.tf
@@ -0,0 +1,58 @@
+resource "google_container_cluster" "no-labels-cluster" {
+  name     = "cluster"
+  location = "us-central1"
+
+  node_config {
+    machine_type = "n1-standard-1"
+    labels       = local.terratag_added_main
+  }
+
+  node_pool {
+    node_config {ll
+      machine_type = "n1-standard-1"
+      labels       = local.terratag_added_main
+    }
+  }
+  resource_labels = local.terratag_added_main
+}
+
+resource "google_container_node_pool" "no-labels-pool" {
+  cluster = google_container_cluster.no-labels-cluster.name
+
+  node_config {
+    machine_type = "n1-standard-1"
+    labels       = local.terratag_added_main
+  }
+}
+
+resource "google_container_cluster" "existing-labels-cluster" {
+  name     = "cluster2"
+  location = "us-central1"
+
+  resource_labels = merge( map("foo" , "bar"), local.terratag_added_main)
+
+  node_config {
+    machine_type = "n1-standard-1"
+    labels       = merge( map("foo" , "bar"), local.terratag_added_main)
+  }
+
+  node_pool {
+    node_config {
+      machine_type = "n1-standard-1"
+      labels       = merge( map("foo" , "bar"), local.terratag_added_main)
+    }
+  }
+}
+
+resource "google_container_node_pool" "existing-labels-pool" {
+  cluster = google_container_cluster.existing-labels-cluster.name
+
+  node_config {
+    machine_type = "n1-standard-1"
+    labels       = merge( map("foo" , "bar"), local.terratag_added_main)
+  }
+}
+locals {
+  terratag_added_main = {"test"="test"}
+}
+
