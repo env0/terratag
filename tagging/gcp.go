9def18ef785d6ea3fc81a0dbc7c557a6706ffbdf
Yaron Yarimi <yaron.yarimi@env0.com>
2020-07-07 15:50:33 +0300

    move repeatable logic into a common func

--- tagging/gcp.go
+++ tagging/gcp.go
@@ -1,13 +1,35 @@
 package tagging
 
 import (
-	"github.com/env0/terratag/terraform"
 	"github.com/hashicorp/hcl/v2/hclwrite"
-	"log"
 )
 
-func tagContainerCluster(args TagBlockArgs) Result {
+func getNodeConfigBlocks(parent *hclwrite.Block) []*hclwrite.Block {
 	var blocks []*hclwrite.Block
+
+	for _, block := range parent.Body().Blocks() {
+		if block.Type() == "node_config" {
+			blocks = append(blocks, block)
+		} else {
+			blocks = append(blocks, getNodeConfigBlocks(block)...)
+		}
+	}
+
+	return blocks
+}
+
+func tagNodeConfigBlocks(args TagBlockArgs) []string {
+	var swappedTagsStrings []string
+
+	for _, block := range getNodeConfigBlocks(args.Block) {
+		args.Block = block
+		swappedTagsStrings = append(swappedTagsStrings, TagBlock(args))
+	}
+
+	return swappedTagsStrings
+}
+
+func tagContainerCluster(args TagBlockArgs) Result {
 	var swappedTagsStrings []string
 
 	// handle root block
@@ -15,40 +37,11 @@ func tagContainerCluster(args TagBlockArgs) Result {
 	rootBlockArgs.TagId = "resource_labels"
 	swappedTagsStrings = append(swappedTagsStrings, TagBlock(rootBlockArgs))
 
-	nodeConfig := terraform.GetNestedBlock(args.Block, "node_config")
-	if nodeConfig != nil {
-		log.Print("Found taggable nested node_config block, processing...")
-		blocks = append(blocks, nodeConfig)
-	}
-
-	nodeConfig = terraform.GetNestedBlock(args.Block, "node_pool", "node_config")
-	if nodeConfig != nil {
-		log.Print("Found taggable nested node_pool/node_config block, processing...")
-		blocks = append(blocks, nodeConfig)
-	}
-
-	for _, block := range blocks {
-		args.Block = block
-		swappedTagsStrings = append(swappedTagsStrings, TagBlock(args))
-	}
+	swappedTagsStrings = append(swappedTagsStrings, tagNodeConfigBlocks(args)...)
 
 	return Result{SwappedTagsStrings: swappedTagsStrings}
 }
 
 func tagContainerNodePool(args TagBlockArgs) Result {
-	var blocks []*hclwrite.Block
-	var swappedTagsStrings []string
-
-	nodeConfig := terraform.GetNestedBlock(args.Block, "node_config")
-	if nodeConfig != nil {
-		log.Print("Found taggable nested node_config block, processing...")
-		blocks = append(blocks, nodeConfig)
-	}
-
-	for _, block := range blocks {
-		args.Block = block
-		swappedTagsStrings = append(swappedTagsStrings, TagBlock(args))
-	}
-
-	return Result{SwappedTagsStrings: swappedTagsStrings}
+	return Result{SwappedTagsStrings: tagNodeConfigBlocks(args)}
 }
