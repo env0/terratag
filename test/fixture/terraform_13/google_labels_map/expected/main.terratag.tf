c0032d167253108424a605a84ccf216f2ad51a1d
Yaron Yarimi <yaron.yarimi@env0.com>
2020-09-13 17:06:28 +0300

    fix tf13 tests

--- test/fixture/terraform_13/google_labels_map/expected/main.terratag.tf
+++ test/fixture/terraform_13/google_labels_map/expected/main.terratag.tf
@@ -23,7 +23,7 @@ resource "google_storage_bucket" "static-site" {
     response_header = ["*"]
     max_age_seconds = 3600
   }
-  labels = merge( map("foo" , "bar"), local.terratag_added_main)
+  labels = merge( map("foo","bar"), local.terratag_added_main)
 }
 locals {
   terratag_added_main = {"env0_environment_id"="40907eff-cf7c-419a-8694-e1c6bf1d1168","env0_project_id"="43fd4ff1-8d37-4d9d-ac97-295bd850bf94"}
