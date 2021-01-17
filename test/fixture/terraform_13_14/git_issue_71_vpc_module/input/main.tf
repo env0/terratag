f9c2a59d76b7dd06a281a8401890f426004bc4d3
avner.sorek <avner.sorek@env0.com>
2021-01-17 09:44:16 +0200

    add tests for popular vpc module

---
+++ test/fixture/terraform_13_14/git_issue_71_vpc_module/input/main.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.47.0"
+  name                 = "vpc-name"
+  cidr                 = "10.0.0.0/16"
+  azs                  = ["123"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster"                       = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster"                       = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
