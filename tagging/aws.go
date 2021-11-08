b89eaa0031f12a15059d814c013b82d47c46b185
roi.ezra <roi.ezra@appsflyer.com>
2021-11-08 15:04:47 +0200

    fixes #101

--- tagging/aws.go
+++ tagging/aws.go
@@ -7,8 +7,13 @@ import (
 func tagAutoscalingGroup(args TagBlockArgs) Result {
 	// for now, we count on it that if there's a single "tag" in the schema (unlike "tags" block),
 	// then no "tags" interpolation is used, but rather multiple instances of a "tag" block
+	// Otherwise, we will append to the existing tags block
 	// https://www.terraform.io/docs/providers/aws/r/autoscaling_group.html
-	convert.AppendTagBlocks(args.Block, args.Tags)
-
+	tagsAttribute := args.Block.Body().GetAttribute("tag")
+	if tagsAttribute != nil {
+		convert.AppendTagBlocks(args.Block, args.Tags)
+	} else {
+		TagBlock(args)
+	}
 	return Result{}
 }
