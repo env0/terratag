1e74442edea9c2767bf9d4a62734cc63f3734e54
Yaron Yarimi <yaron.yarimi@env0.com>
2021-11-21 12:07:53 +0200

    remove bucket_policy_only attribute

--- test/fixture/terraform_11/google_labels_map/input/main.tf
+++ test/fixture/terraform_11/google_labels_map/input/main.tf
@@ -3,8 +3,6 @@ resource "google_storage_bucket" "static-site" {
   location      = "EU"
   force_destroy = true
 
-  bucket_policy_only = true
-
   website {
     main_page_suffix = "index.html"
     not_found_page   = "404.html"
