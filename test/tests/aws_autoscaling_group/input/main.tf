71ee4e4b9b6951b9e98e504f3a054211bcfa84c0
Tomer Heber <theber@zscaler.com>
2022-07-26 09:35:56 -0500

    updated tests

--- test/tests/aws_autoscaling_group/input/main.tf
+++ test/tests/aws_autoscaling_group/input/main.tf
@@ -57,3 +57,44 @@ EOF
     propagate_at_launch = false
   }
 }
+
+resource "aws_autoscaling_group" "bar_tags" {
+  name                      = "foobar3-terraform-test"
+  max_size                  = 5
+  min_size                  = 2
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 4
+  force_delete              = true
+
+  initial_lifecycle_hook {
+    name                 = "foobar"
+    default_result       = "CONTINUE"
+    heartbeat_timeout    = 2000
+    lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
+
+    notification_metadata = <<EOF
+{
+  "foo": "bar"
+}
+EOF
+
+    notification_target_arn = "arn:aws:sqs:us-east-1:444455556666:queue1*"
+    role_arn                = "arn:aws:iam::123456789012:role/S3Access"
+  }
+
+  timeouts {
+    delete = "15m"
+  }
+
+  tags = [
+    {
+      key   = "a"
+      value = "b"
+    },
+    {
+      key   = "c"
+      value = "d"
+    }
+  ]
+}
