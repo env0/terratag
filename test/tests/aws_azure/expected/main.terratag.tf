e96f7f6a02f4766bcdf1a8f1a6dea9496bdab58b
Tomer Heber <theber@zscaler.com>
2023-03-10 20:30:42 -0600

    fix test

--- test/tests/aws_azure/expected/main.terratag.tf
+++ test/tests/aws_azure/expected/main.terratag.tf
@@ -45,3 +45,4 @@ resource "aws_s3_bucket" "should_have_tags" {
 locals {
   terratag_added_main = {"env0_environment_id"="40907eff-cf7c-419a-8694-e1c6bf1d1168","env0_project_id"="43fd4ff1-8d37-4d9d-ac97-295bd850bf94"}
 }
+
