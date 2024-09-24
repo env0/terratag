355cb79963f3bd10a2abd2d6f65a15982f120597
Tomer Heber <theber@zscaler.com>
2024-09-24 09:53:41 -0500

    update test

--- test/tests/google_vs_google_beta/input/main.tf
+++ test/tests/google_vs_google_beta/input/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "4.85.0"
+      version = "4.84.0"
     }
     google-beta = {
       source = "hashicorp/google-beta"
