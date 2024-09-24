b8191e1f4a0ffafbb00553b36b16c2c0ff4c6716
Tomer Heber <theber@zscaler.com>
2024-09-24 11:25:59 -0500

    add test and update versions

--- test/tests/provider_function/expected/main.terratag.tf
+++ test/tests/provider_function/expected/main.terratag.tf
@@ -21,5 +21,5 @@ output "hashicups_ecr_repository_account_id" {
 }
 
 locals {
-   terratag_added_main = {"env0_environment_id"="40907eff-cf7c-419a-8694-e1c6bf1d1168","env0_project_id"="43fd4ff1-8d37-4d9d-ac97-295bd850bf94"}
+  terratag_added_main = {"env0_environment_id"="40907eff-cf7c-419a-8694-e1c6bf1d1168","env0_project_id"="43fd4ff1-8d37-4d9d-ac97-295bd850bf94"}
 }
